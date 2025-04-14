68a77f7a701c764ca1d6bb248537f88f738ceda1
bluepartyballoon <amy.jereczek@gmail.com>
2025-04-14 09:55:38 +0200

    Add-Funktion zur Klasse Attack hinzugefügt

--- src/main/kotlin/hwr/oop/classes/Attack.kt
+++ src/main/kotlin/hwr/oop/classes/Attack.kt
@@ -1,5 +1,13 @@
 package hwr.oop.classes
 
 class Attack(val name : String, val type: Type, val damage: Int, val accuracy: Int ) {
+    companion object {
+        private val allAttacks = mutableListOf<Attack>()
 
+        fun register(attack: Attack) {
+            allAttacks.add(attack)
+        }
+
+        fun getAll(): List<Attack> = allAttacks.toList()
+    }
 }
