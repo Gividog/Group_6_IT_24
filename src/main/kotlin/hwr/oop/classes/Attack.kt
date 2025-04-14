22b6856df3de6d7dd5b2862bec12500b6d2b2504
liesapoetsch <liesa@w-poetsch.de>
2025-04-14 10:15:47 +0200

    Added AttackInitializer

--- src/main/kotlin/hwr/oop/classes/Attack.kt
+++ src/main/kotlin/hwr/oop/classes/Attack.kt
@@ -1,5 +1,5 @@
 package hwr.oop.classes
 
-class Attack(val name : String, val type: Type, val damage: Int, val accuracy: Int ) {
+class Attack(val name : String, val type: Type, val power: Int, val accuracy: Int ) {
 
 }
