91eb6b2ab83686505aa9d7b5589bc6a5311e09ca
liesapoetsch <liesa@w-poetsch.de>
2025-05-16 19:58:24 +0200

    Added Function to check if Battle is Finished because no monsters left

--- src/main/kotlin/monsterleague/gamelogic/Battle.kt
+++ src/main/kotlin/monsterleague/gamelogic/Battle.kt
@@ -7,4 +7,20 @@ class Battle(
     val trainers: List<Trainer>
 ) {
 
+fun proofIfBattleIsFinished():Boolean{
+    return trainers[0].monsters.isEmpty() || trainers[1].monsters.isEmpty()
+    //determineWinner()
+    //saveBattleInformation()
+}
+
+    //TODO: Gewinner festlegen
+    fun determineWinner(){
+
+    }
+
+    //TODO: Kampf Informationen speichern
+    fun saveBattleInformation(){
+
+    }
+
 }
